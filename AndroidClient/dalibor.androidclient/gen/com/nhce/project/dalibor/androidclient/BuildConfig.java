10a8c3d7a8bc3d4a8646c7656c6041e25b43635c
jonathangizmo <jonathangizmo@gmail.com>
2013-02-15 07:33:41 +0530

    initialized android client

---
+++ AndroidClient/dalibor.androidclient/gen/com/nhce/project/dalibor/androidclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.nhce.project.dalibor.androidclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
