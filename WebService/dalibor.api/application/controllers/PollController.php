c24f39fdee76e116bd6cc182e85e74d4d8baea97
jonathangizmo <jonathangizmo@gmail.com>
2013-02-16 07:45:19 +0530

    added poll REST controller

---
+++ WebService/dalibor.api/application/controllers/PollController.php
@@ -0,0 +1,31 @@
+<?php
+
+class PollController extends Zend_Rest_Controller {
+
+    public function getAction() {
+        
+    }
+
+    public function headAction() {
+        
+    }
+
+    public function indexAction() {
+        
+    }
+
+    public function postAction() {
+        
+    }
+
+    public function putAction() {
+        
+    }
+
+    public function deleteAction() {
+        
+    }
+
+}
+
+?>
