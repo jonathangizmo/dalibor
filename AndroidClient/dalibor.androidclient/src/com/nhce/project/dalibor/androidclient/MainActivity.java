a7fa9b78a1a7b747ed2a1e2b140d1a795829c47f
jonathangizmo <jonathangizmo@gmail.com>
2013-04-27 09:57:50 +0530

    trying to connect to network

--- AndroidClient/dalibor.androidclient/src/com/nhce/project/dalibor/androidclient/MainActivity.java
+++ AndroidClient/dalibor.androidclient/src/com/nhce/project/dalibor/androidclient/MainActivity.java
@@ -1,18 +1,21 @@
 package com.nhce.project.dalibor.androidclient;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.HttpEntityWrapper;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,6 +25,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private static final String USER_CONTROLLER = "/user/";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,15 +54,52 @@ public class MainActivity extends Activity {
 				String sPort = etPort.getText().toString();
 				String sUsername = etUsername.getText().toString();
 				String sPassword = etPassword.getText().toString();
-				HttpClient httpclient = new DefaultHttpClient();
-			    HttpPost httppost = new HttpPost(sHost+":"+sPort);
-			    String sJsonData ="'{\"username\":\""+sUsername+"\",\"password\":\""+sPassword+"\"}'";
-
+				connectToServer(sHost,sPort,sUsername,sPassword);
 			}
 		});
     }
 
-    @Override
+    protected void connectToServer(final String sHost, final String sPort, final String sUsername, final String sPassword) {
+    	 Thread t = new Thread() {
+
+             public void run() {
+                 Looper.prepare();
+                 HttpClient client = new DefaultHttpClient();
+                 HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
+                 HttpResponse response;
+                 JSONObject json = new JSONObject();
+                 String URL = sHost + ":" + sPort;
+                 URL+=USER_CONTROLLER;
+
+                 try {
+                     HttpPost post = new HttpPost(URL);
+                     json.put("username", sUsername);
+                     json.put("password", sPassword);
+                     
+                     StringEntity se = new StringEntity( json.toString());  
+                     se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                     post.setEntity(se);
+                     response = client.execute(post);
+
+                     if(response!=null){
+                         InputStream in = response.getEntity().getContent();
+                         Log.i("response", in.toString());
+                         Toast.makeText(getApplicationContext(), "Recieved response", Toast.LENGTH_LONG).show();
+                     }
+
+                 } catch(Exception e) {
+                     e.printStackTrace();
+                     Log.e("Error", "Cannot Estabilish Connection");
+                 }
+
+                 Looper.loop();
+             }
+         };
+
+         t.start();
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
